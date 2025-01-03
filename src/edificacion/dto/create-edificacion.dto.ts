c05dbcdf9b98d0a55384022f7b5f138673179912
Adrian <adrian.suarez.alphen@gmail.com>
2025-01-02 23:52:47 -0500

    fix: buildings module removed

--- src/edificacion/dto/create-edificacion.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateEdificacionDto {
-    nombre: string
-}
--- src/edificacion/dto/update-edificacion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEdificacionDto } from './create-edificacion.dto';
-
-export class UpdateEdificacionDto extends PartialType(CreateEdificacionDto) {}
