21890e5c698293aef3e0743cf61234721df69953
Adrian <adrian.suarez.alphen@gmail.com>
2025-01-03 00:03:14 -0500

    fix(app-module): delete imports of buildings module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EdificacionModule } from './edificacion/edificacion.module';
 import { InspeccionModule } from './inspeccion/inspeccion.module';
 
 @Module({
@@ -10,7 +9,6 @@ import { InspeccionModule } from './inspeccion/inspeccion.module';
     MongooseModule.forRoot(
       `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`,
     ),
-    EdificacionModule,
     InspeccionModule,
   ],
   controllers: [AppController],
