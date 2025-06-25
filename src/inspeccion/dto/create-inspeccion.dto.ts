0fe76d921537832fac1890573aa87d403661051f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-25 01:01:20 -0400

    feat(create-inspection-dto): add property id

--- src/inspeccion/dto/create-inspeccion.dto.ts
+++ src/inspeccion/dto/create-inspeccion.dto.ts
@@ -2,6 +2,7 @@ import { CreateDeterioroDTO } from "../deterioro/dto/create-deterioro.dto";
 
 
 export class CreateInspeccionDto {
+    id?: string
     configId: string // representa la configuración con la que fue realiza la inspección
     edificacionId: string
     deterioros: Array<CreateDeterioroDTO>
